b94878f1c4f668cb6e346a83a80ebe40ae5f719e
Mikael Berglund <mikbe@kth.se>
2017-11-23 23:13:55 +0100

    Removed server or client

--- notification-service-server/src/main/java/com/valhallagame/notificationservice/model/NotificationType.java
+++
@@ -1,9 +0,0 @@
-package com.valhallagame.notificationservice.model;
-
-public enum NotificationType {
-	PARTYCHANGE, DUNGEONCHANGE, FRIENDCHANGE, REWARDCHANGE, CHATCHANGE, PERSONCHANGE;
-
-	public String getValue() {
-		return this.name();
-	}
-}
