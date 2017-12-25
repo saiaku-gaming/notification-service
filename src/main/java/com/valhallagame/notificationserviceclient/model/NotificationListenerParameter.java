31113809b35cb6b77b81dfc8fe554202a73d7404
Mikael Berglund <mikbe@kth.se>
2017-12-25 22:31:10 +0100

    More validation

--- src/main/java/com/valhallagame/notificationserviceclient/model/NotificationListenerParameter.java
+++ src/main/java/com/valhallagame/notificationserviceclient/model/NotificationListenerParameter.java
@@ -1,5 +1,7 @@
 package com.valhallagame.notificationserviceclient.model;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NotificationListenerParameter {
+	@NotNull
 	private String gameSessionId;
+	
+	@NotNull
 	private String address;
+
+	@NotNull
 	private int port;
 }
